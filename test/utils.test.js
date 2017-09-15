4795b7b000bc79128d27c2a6a5f4e01cdd5ade99
thyagoluciano <thyagoluciano@gmail.com>
2017-09-15 12:12:13 -0300

    atualizar script travis

--- test/utils.test.js
+++ test/utils.test.js
@@ -6,17 +6,6 @@ describe('e2e.storage', () => {
         expect(Utils.getContentType(JSON.stringify(objeto))).toBe('json');
     });
 
-    test('should be return the xml type', () => {
-        const xml = `
-        <?xml version="1.0" encoding="UTF-8"?>
-        <aluno situacao="Não Matriculado">
-            <nome>Tadeu</nome>
-            <idade>42</idade>
-        </aluno>`;
-
-        expect(Utils.getContentType(xml).toBe('json'));
-    });
-
     test('Should be convert to base64', () => {
         expect(Utils.base64Encode('password')).toBe('cGFzc3dvcmQ=');
     });
